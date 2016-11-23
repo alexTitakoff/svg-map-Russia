f6caace3b48d2915634141caacc4e3ff862c9fe0
alex <alex.iTakov@yandex.ru>
2016-11-23 20:29:54 +0300

    0.3v

---
+++ README.txt
@@ -0,0 +1,9 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/eltonkamami/pen/zglJE.
+
+ Watch in full view
+
+The svg map was taken from here  <a href='goo.gl/Ep0xP'>svg map</a> from <a href='https://twitter.com/f1lt3r'>@f1lt3r</a>
+
+Uses www.geonames.org API which has a 30k hits per day and 2k hits per hour limit.If it doesnt work we got there
+
+I tried to list most of the countries.
---
+++ js/index.js
@@ -0,0 +1,238 @@
+
+// function numberFormat(nr){
+// 	//remove the existing ,
+// 	var regex = /,/g;
+// 	nr = nr.replace(regex,'');
+// 	//force it to be a string
+// 	nr += '';
+// 	//split it into 2 parts  (for numbers with decimals, ex: 125.05125)
+// 	var x = nr.split('.');
+// 	var p1 = x[0];
+// 	var p2 = x.length > 1 ? '.' + x[1] : '';
+// 	//match groups of 3 numbers (0-9) and add , between them
+// 	regex = /(\d+)(\d{3})/;
+// 	while (regex.test(p1)) {
+// 		p1 = p1.replace(regex, '$1' + ',' + '$2');
+// 	}
+// 	//join the 2 parts and return the formatted number
+// 	return p1 + p2;
+// }
+
+// function update(json){
+//   country = json['geonames'][0];
+
+//   if(country === undefined){
+//     $('.indicator').html('Information could not be found').css('width','300px');
+
+//   }else{
+
+//     $('.indicator').html('').append('<h2 class="name">'+country.countryName+'</h2>');
+//     $('.indicator').append('<img src="http://www.geonames.org/flags/m/'+country.countryCode.toLowerCase()+'.png" alt="" class="flag" />');
+
+//     $('.indicator').append('<div class="clear capital">Capital City : '+country.capital+'</div>');
+
+//     $('.indicator').append('<div class="population">Population : '+numberFormat(country.population)+'</div>');
+
+//     if(country.countryName== 'Lebanon')country.areaInSqKm = '10452';
+
+//     $('.indicator').append('<div class="area">Area in km&sup2; : '+ numberFormat(country.areaInSqKm)+ '</div>');
+
+//   }
+// }
+
+
+$('path').hover(function(e){
+
+  $('path').css('fill','#fff');
+  $('.indicator').html('');
+  var id = $(this).attr('id').toUpperCase();
+
+  if($(this).attr('name')) {
+    var name = $(this).attr('name');
+    $('<div>' + name +'</div>').appendTo('.indicator');
+  }
+
+
+  if($(this).attr('flag')) {
+    var flag = "http://sandbox.alexti-dev.ru/mapping/" + $(this).attr('flag') ;
+    $(' <img class="flag" src="" alt="">').appendTo('.indicator');
+    $('.indicator').find('img').attr('src',flag )
+   // $('<img src='+ flag+ ' >').appendTo('.indicator');
+ }
+
+ $('.change').remove();
+
+ var script = document.createElement('script');
+ // script.classList.add('change');
+  //script.src = 'http://api.geonames.org/countryInfoJSON?country='+info[id]+'&username=pixeltest&style=full&callback=update';
+  document.body.appendChild(script);
+
+  $(this).css('fill','#f6e72d');
+  $('path').not(this).css('fill','rgba(0,0,0,0.5)');
+  $('.indicator').css({'top':e.pageY,'left':e.pageX+30}).show();
+
+
+
+},function(){
+  $('.indicator').html('');
+  $('.indicator').hide();
+  $('path').css('fill','rgba(0,0,0,0.2)');
+});
+
+
+
+
+
+var idAarr = ["RU-MOW", "RU-SPE", "RU-NEN", "RU-YAR", "RU-CHE", "RU-ULY", "RU-TYU", "RU-TUL", "RU-SVE", "RU-RYA", "RU-ORL", "RU-OMS", "RU-NGR", "RU-LIP", "RU-KRS", "RU-KGN", "RU-KGD", "RU-IVA", "RU-BRY", "RU-AST", "RU-KHA", "RU-CE", "RU-UD", "RU-SE", "RU-MO", "RU-KR", "RU-KL", "RU-IN", "RU-AL", "RU-BA", "RU-AD", "RU-CR", "RU-SEV", "RU-KO", "RU-KIR", "RU-PNZ", "RU-TAM", "RU-MUR", "RU-LEN", "RU-VLG", "RU-KOS", "RU-PSK", "RU-ARK", "RU-YAN", "RU-CHU", "RU-YEV", "RU-TY", "RU-SAK", "RU-AMU", "RU-BU", "RU-KK", "RU-KEM", "RU-NVS", "RU-ALT", "RU-DA", "RU-STA", "RU-KB", "RU-KC", "RU-KDA", "RU-ROS", "RU-SAM", "RU-TA", "RU-ME", "RU-CU", "RU-NIZ", "RU-VLA", "RU-MOS", "RU-KLU", "RU-BEL", "RU-ZAB", "RU-PRI", "RU-KAM", "RU-MAG", "RU-SA", "RU-KYA", "RU-ORE", "RU-SAR", "RU-VGG", "RU-VOR", "RU-SMO", "RU-TVE", "RU-PER", "RU-KHM", "RU-TOM", "RU-IRK"];
+var idAarr2 = new Array(
+  ["RU-MOW",  "Москва", "moscow.gif"],
+  ["RU-CHE", "Челябинская область", "chelyabinskaya_flag.png" ],
+  ["RU-ORL",  "Орловская область"],
+  ["RU-OMS",  "Омская область", "flag_omskoj_oblasti.png"],
+  ["RU-LIP",  "Липецкая область", "lipeckya.jpg"],
+  ["RU-KRS",  "Курская область", "flag_of_kursk_oblast.png"],
+  ["RU-RYA",  "Рязанская область", "ryazan.png"],
+  ["RU-BRY",  "Брянская область", "bryanskaya_flag.png"],
+  ["RU-KIR",  "Кировская область", "flag_kirovskoy_oblasti.png"],
+  ["RU-ARK",  "Архангельская область", ""],
+  ["RU-MUR",  "Мурманская область", ""],
+  ["RU-SPE",  "Санкт-Петербург", ""],
+  ["RU-YAR",  "Ярославская область", ""],
+  ["RU-ULY",  "Ульяновская область", ""],
+  ["RU-NVS",  "Новосибирская область", ""],
+  ["RU-TYU",  "Тюменская область", ""],
+  ["RU-SVE",  "Свердловская область", ""],
+  ["RU-NGR",  "Новгородская область", ""],
+  ["RU-KGN",  "Курганская область", ""],
+  ["RU-KGD",  "Калининградская область", ""],
+  ["RU-IVA",  "Ивановская область", ""],
+  ["RU-AST",  "Астраханская область", ""],
+  ["RU-KHA",  "Хабаровский край", ""],
+  ["RU-CE",  "Чеченская республика", ""],
+  ["RU-UD",  "Удмуртская республика", ""],
+  ["RU-SE",  "Республика Северная Осетия", ""],
+  ["RU-MO",  "Республика Мордовия", ""],
+  ["RU-KR",  "Республика  Карелия", ""],
+  ["RU-KL",  "Республика  Калмыкия", ""],
+  ["RU-IN",  "Республика  Ингушетия", ""],
+  ["RU-AL",  "Республика Алтай", ""],
+  ["RU-BA",  "Республика Башкортостан", ""],
+  ["RU-AD",  "Республика Адыгея", ""],
+  ["RU-CR",  "Республика Крым", ""],
+  ["RU-SEV",  "Севастополь", ""],
+  ["RU-KO",  "Республика Коми", ""],
+  ["RU-PNZ",  "Пензенская область", ""],
+  ["RU-TAM",  "Тамбовская область", ""],
+  ["RU-LEN",  "Ленинградская область", ""],
+  ["RU-VLG",  "Вологодская область", ""],
+  ["RU-KOS",  "Костромская область", ""],
+  ["RU-PSK",  "Псковская область", ""],
+  ["RU-YAN",  "Ямало-Ненецкий АО", ""],
+  ["RU-CHU",  "Чукотский АО", ""],
+  ["RU-YEV",  "Еврейская автономская область", ""],
+  ["RU-TY",  "Республика Тыва", ""],
+  ["RU-SAK",  "Сахалинская область", ""],
+  ["RU-AMU",  "Амурская область", ""],
+  ["RU-BU",  "Республика Бурятия", ""],
+  ["RU-KK",  "Республика Хакасия", ""],
+  ["RU-KEM",  "Кемеровская область", ""],
+  ["RU-ALT",  "Алтайский край", ""],
+  ["RU-DA",  "Республика Дагестан", ""],
+  ["RU-KB",  "Кабардино-Балкарская республика", ""],
+  ["RU-KC",  "Карачаевая-Черкесская республика", ""],
+  ["RU-KDA",  "Краснодарский край", ""],
+  ["RU-ROS",  "Ростовская область", ""],
+  ["RU-SAM",  "Самарская область", ""],
+  ["RU-TA",  "Республика Татарстан", ""],
+  ["RU-ME",  "Республика Марий Эл", ""],
+  ["RU-CU",  "Чувашская республика", ""],
+  ["RU-NIZ",  "Нижегородская край", ""],
+  ["RU-VLA",  "Владимировская область", ""],
+  ["RU-MOS",  "Московская область", ""],
+  ["RU-KLU",  "Калужская область", ""],
+  ["RU-BEL",  "Белгородская область", ""],
+  ["RU-ZAB",  "Забайкальский край", ""],
+  ["RU-PRI",  "Приморский край", ""],
+  ["RU-KAM",  "Камачатский край", ""],
+  ["RU-MAG",  "Магаданская область", ""],
+  ["RU-SA",  "Республика Саха", ""],
+  ["RU-KYA",  "Красноярский край", ""],
+  ["RU-ORE",  "Оренбургская область", ""],
+  ["RU-SAR",  "Саратовская область", ""],
+  ["RU-VGG",  "Волгоградская область", ""],
+  ["RU-VOR",  "Ставропольский край", ""],
+  ["RU-SMO",  "Смоленская область", ""],
+  ["RU-TVE",  "Тверская область", ""],
+  ["RU-PER",  "Пермская область", ""],
+  ["RU-KHM",  "Ханты-Мансийский АО", ""],
+  ["RU-KHM",  "Ханты-Мансийский АО", ""],
+  ["RU-TOM",  "Томская область", ""],
+  ["RU-IRK",  "Иркутская область", ""],
+  ["RU-NEN",  "Ненецскй АО", ""],
+  ["RU-STA",  "Ставропольский край", ""],
+  ["RU-TUL",  "Тульская область", "tulskaya_flag.png"]
+
+  );
+
+$('path').each(function() {
+
+  var regId = $(this).attr('id');
+  var flag = '';
+  var name = '';
+  for (var j = 0; j < idAarr2.length; j++) {
+
+    if (regId == idAarr2[j][0]) {
+      name = idAarr2[j][1];
+      flag =  'flags/'+ idAarr2[j][2];
+
+      $(this).attr('name', name);
+      $(this).attr('flag', flag);
+    }
+  }
+
+
+  var regIdDiv = '<div class="reg" >'+ '[' + '<span>'+  regId +'</span>' + ']' +' '+ name +'</div>'
+  $(regIdDiv).appendTo('.regs');
+
+
+// var idArrMin = [regId, '_'];
+// idAarr2.push(idArrMin);
+
+
+})
+
+
+// for (var j = 0; j < idAarr2.length; j++) {
+//   if (regId == idAarr2[j][0]) {
+//     name = cyr2latChars[j][1];
+
+//   }
+// }
+
+
+function naming() {
+
+}
+
+
+//revertFill();
+
+
+$('.reg').hover(function(e) {
+
+
+  var id = $(this).find('span').text();
+
+  idHover = '#' + id;
+
+  $(idHover).css('fill','#f6e72d');
+ // $('path').not(this).css('fill','rgba(0,0,0,0.5)');
+ // $('.indicator').css({'top':e.pageY,'left':e.pageX+30}).show();
+
+},function(){
+  $('.indicator').html('');
+  $('.indicator').hide();
+  $('path').css('fill','rgba(0,0,0,0.2)');
+});
+
+//} // revertFill
